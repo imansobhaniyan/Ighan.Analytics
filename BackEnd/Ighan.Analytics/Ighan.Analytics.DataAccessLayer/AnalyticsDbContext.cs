827bc8e11cebc65adb063847e0309f6d5d290b17
Iman Sobhaniyan <sobhaniyan.iman@gmail.com>
2021-06-01 00:53:31 +0430

    add db context class

---
+++ BackEnd/Ighan.Analytics/Ighan.Analytics.DataAccessLayer/AnalyticsDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Ighan.Analytics.StorageModels;
+
+using Microsoft.EntityFrameworkCore;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ighan.Analytics.DataAccessLayer
+{
+    public class AnalyticsDbContext : DbContext
+    {
+        public AnalyticsDbContext(DbContextOptions<AnalyticsDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+
+        public DbSet<Project> Projects { get; set; }
+
+        public DbSet<AnalyticKey> Keys { get; set; }
+
+        public DbSet<AnalyticValue> Values { get; set; }
+    }
+}
