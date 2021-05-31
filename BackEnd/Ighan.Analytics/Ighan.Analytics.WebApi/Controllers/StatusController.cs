fdfa15cbe8ef2ed3733f6b6c46dcd7eb68a8abec
Iman Sobhaniyan <sobhaniyan.iman@gmail.com>
2021-06-01 01:03:53 +0430

    add status controller

---
+++ BackEnd/Ighan.Analytics/Ighan.Analytics.WebApi/Controllers/StatusController.cs
@@ -0,0 +1,32 @@
+﻿using Ighan.Analytics.DataAccessLayer;
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ighan.Analytics.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class StatusController : ControllerBase
+    {
+        private readonly AnalyticsDbContext dbContext;
+
+        public StatusController(AnalyticsDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public async Task<string> Get()
+        {
+            await dbContext.Users.FirstOrDefaultAsync();
+
+            return "I`m up and running";
+        }
+    }
+}
